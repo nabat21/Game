2a22e20b1b7dc5f363f7e14aa519399d65aa5c3e
nabat21 <76848446+nabat21@users.noreply.github.com>
2022-01-03 21:28:34 +0330

    Update game.py

--- game.py
+++ game.py
@@ -1,18 +1,35 @@
+import random
 
-player_1 = input("start the player_1:")
-player_2 = input("start the player_2:")
+random_number = random.randint(0, 2)
+computer_move = "rock"
 
-if player_1 == "rock" and player_2 == "scissor":
-    print("You won the player_1")
-elif player_1 == "rock" and player_2 == "paper":
-    print("You won the player_2")
-elif player_1 == "paper" and player_2 == "rock":
-    print("You won the player_1")
-elif player_1 == "paper" and player_2 == "scissor":
-    print("You won the player_2")
-elif player_1 == "scissor" and player_2 == "paper":
-    print("You won the player_1")
-elif player_1 == "scissor" and player_2 == "rock":
-    print("You won the player_2")
-elif player_1 == player_2:
-    print("You became equal...  :)")
+if random_number == 0:
+    computer_move = "rock"
+elif random_number == 1:
+    computer_move = "paper"
+elif random_number == 2:
+    computer_move = "scissor"
+
+player_1 = input("start the player_1:").lower()
+player_2 = computer_move
+
+
+if player_1 == player_2:
+    print("You became equal...")
+elif player_1 == "rock":
+    if player_2 == "scissor":
+        print("You won the player_1")
+    elif player_2 == "paper":
+        print("You won the player_2")
+elif player_1 == "paper":
+    if player_2 == "rock":
+        print("You won the player_1")
+    elif player_2 == "scissor":
+        print("You won the player_2")
+elif player_1 == "scissor":
+    if player_2 == "paper":
+        print("You won the player_1")
+    elif player_2 == "rock":
+        print("You won the player_2")
+else:
+    print("something went wrong...")
